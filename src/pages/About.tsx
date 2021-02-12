f83d7a9a209d27a5e34a3843e2993e4b5772b9d7
Ali <ali.hussein.pre@gmail.com>
2021-02-12 11:28:13 +0100

    fix (UI)

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,12 +5,12 @@ const links = [
   {
     href: 'https://twitter.com/AliHussein_20',
     IconName: FaTwitter,
-    cn: 'text-blue-500',
+    cn: 'text-blue-500 ',
   },
   {
     href: 'https://github.com/Ali-Hussein-dev',
     IconName: FaGithub,
-    cn: 'text-gray-600',
+    cn: 'text-gray-600 ',
   },
   {
     href: 'mailto:ali.hussein.pre@gmail.com',
@@ -22,7 +22,7 @@ interface LinkProp {
   href: string
   cn: string
   IconName?: IconType
-  children?: string
+  children?: any
 }
 const Link: React.FC<LinkProp> = ({ href, IconName, cn, children }) => (
   <div>
@@ -58,27 +58,30 @@ const About: React.FC = () => {
         <h2 className="font-semibold ">My Stack</h2>
         <p className="pb-2 mb-4 leading-8 border-b border-gray-200">
           TypeScript, Javascript, React, Next.js, CSS, Html, Jest, Tailwindcss,
-          Git, xState <br /> Familiar with <br /> Nodejs and MongoDB
+          Git, xState <br /> Familiar with <br />
+          Nodejs and MongoDB
         </p>
-        <div className="flex py-3 border-b border-gray-300 gap-x-4">
+        <div className="flex py-3 border-b border-gray-300">
           {links.map((o, i) => (
-            <span key={i}>
+            <span key={i} className="p-1 ml-3 rounded-xl hover:bg-blueGray-100">
               <Link href={o.href} cn={o.cn} IconName={o.IconName} />
             </span>
           ))}
-          <span>
-            <svg
-              className="fill-current w-44 "
-              viewBox="0 0 688 118"
-              fill="none"
-            >
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M7.95 38.655c-10.6 10.6-10.6 27.784 0 38.383l30.705 30.706c10.6 10.599 27.784 10.599 38.383 0l30.706-30.706c10.599-10.6 10.599-27.784 0-38.383L77.038 7.95c-10.6-10.599-27.784-10.599-38.383 0L7.95 38.655zm63.33 32.626c7.42-7.42 7.42-19.449 0-26.868-7.419-7.42-19.448-7.42-26.867 0-7.42 7.42-7.42 19.448 0 26.868 7.42 7.419 19.448 7.419 26.868 0z"
-                fill="#2962FF"
-              ></path>
-            </svg>
+          <span className="p-1 ml-3 rounded-xl hover:bg-blueGray-50">
+            <Link href={'https://hashnode.com/@Ali-2021'} cn="">
+              <svg
+                className="w-8 fill-current"
+                viewBox="0 0 118 118"
+                fill="none"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M7.95 38.655c-10.6 10.6-10.6 27.784 0 38.383l30.705 30.706c10.6 10.599 27.784 10.599 38.383 0l30.706-30.706c10.599-10.6 10.599-27.784 0-38.383L77.038 7.95c-10.6-10.599-27.784-10.599-38.383 0L7.95 38.655zm63.33 32.626c7.42-7.42 7.42-19.449 0-26.868-7.419-7.42-19.448-7.42-26.867 0-7.42 7.42-7.42 19.448 0 26.868 7.42 7.419 19.448 7.419 26.868 0z"
+                  fill="#2962FF"
+                ></path>
+              </svg>
+            </Link>
           </span>
         </div>
       </div>
