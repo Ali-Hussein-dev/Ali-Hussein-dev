966701008c98de6463255706ccae994b36d9d2c1
Ali <ali.hussein.pre@gmail.com>
2021-02-12 10:24:56 +0100

    feat(UI) set global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,25 @@
+import { Header } from '@/components/Header'
+import Head from 'next/head'
+import { ChakraProvider } from '@chakra-ui/react'
+
 import '../css/global.css'
 const MyApp: React.FC<{ Component: React.FC; pageProps: any }> = ({
   Component,
   pageProps,
 }): JSX.Element => {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <main className="text-blueGray-600">
+        <Head>
+          <link rel="icon" href="/favicon-32x32.png" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Ali Hussein</title>
+        </Head>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
