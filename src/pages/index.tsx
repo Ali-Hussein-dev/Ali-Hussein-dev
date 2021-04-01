60562b7d61d2243d31a2cf73ab59b3235218461a
Ali <ali.hussein.pre@gmail.com>
2021-04-01 23:54:48 +0200

    feat(homepage)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ export const Home = (): JSX.Element => (
   <section className="flex flex-col items-center justify-center h-screen px-2">
     <div>
       <h1 className="text-2xl font-black text-teal-400 lg:text-4xl">
-        Welcome To My Portfolio
+        Welcome To My Site!
       </h1>
       <p className="lg:text-lg text-trueGray-500">
         Check other sections to see more
