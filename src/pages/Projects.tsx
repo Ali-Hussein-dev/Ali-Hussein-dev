80a5c0e9a21f14962c5c1c39b1571e68fe9c1c47
Ali <ali.hussein.pre@gmail.com>
2021-11-11 07:43:18 +0100

    refactor: update project url, code style

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,7 +4,7 @@ import { FiExternalLink } from 'react-icons/fi'
 
 const projects = [
   {
-    href: 'https://kartebuch.vercel.app/clients/muster/normal',
+    href: 'https://kartebuch.vercel.app/clients/101/normal',
     name: 'Interactive-menu',
     description:
       'Dishes/drinks menu: digital menu for restaurants \nFeatures: dynamically-visible items, easy to navigate through items',
@@ -44,65 +44,58 @@ const ProjectCard: React.FC<{
   description: string
   src: string
   vidType: string
-}> = ({ name, href, description, stack, src, vidType }) => {
-  return (
-    <div className="flex flex-col">
-      {src && (
-        <video className="mb-3 rounded-lg max-h-80" controls>
-          <source src={src} type={vidType} />
-        </video>
-      )}
-      <div className="pb-1 mb-4 border-b border-trueGray-200">
-        <h2 className="mb-1 font-semibold uppercase">{name}</h2>
-        <p className="">{description}</p>
-        <p className="text-blueGray-400">
-          <span className="font-semibold text-blueGray-700">Stack</span>:{' '}
-          {stack}
-        </p>
-        <Link href={href}>demo</Link>
-      </div>
+}> = ({ name, href, description, stack, src, vidType }) => (
+  <div className="flex flex-col">
+    {src && (
+      <video className="mb-3 rounded-lg max-h-80" controls>
+        <source src={src} type={vidType} />
+      </video>
+    )}
+    <div className="pb-1 mb-4 border-b border-trueGray-200">
+      <h2 className="mb-1 font-semibold uppercase">{name}</h2>
+      <p className="">{description}</p>
+      <p className="text-blueGray-400">
+        <span className="font-semibold text-blueGray-700">Stack</span>: {stack}
+      </p>
+      <Link href={href}>demo</Link>
     </div>
-  )
-}
+  </div>
+)
 const Link: React.FC<{ href: string; children: string }> = ({
   href,
   children,
-}) => {
-  return (
-    <a
-      href={href}
-      target="_blank"
-      rel="noreferrer"
-      className="flex items-center w-24 mt-2 gap-1 text-blue-500 hover:text-blue-400"
-    >
-      {children} <FiExternalLink size="15" className="text-trueGray-400" />
-    </a>
-  )
-}
+}) => (
+  <a
+    href={href}
+    target="_blank"
+    rel="noreferrer"
+    className="flex items-center w-24 mt-2 gap-1 text-blue-500 hover:text-blue-400"
+  >
+    {children} <FiExternalLink size="15" className="text-trueGray-400" />
+  </a>
+)
 //=======================
-const Projects: React.FC = () => {
-  return (
-    <section id="Projects" className="grid px-3 pt-6 place-items-center ">
-      <Head>
-        <title>Projects</title>
-      </Head>
-      <div className="py-4 md:w-8/12 lg:w-6/12">
-        <h1 className="pb-1 mb-4 text-lg font-bold text-teal-500 border-b-4 border-teal-400">
-          PROJECTS
-        </h1>
-        {projects.map((o, i) => (
-          <ProjectCard
-            key={i}
-            name={o.name}
-            href={o.href}
-            description={o.description}
-            stack={o.stack}
-            src={o.src}
-            vidType={o.vidType}
-          />
-        ))}
-      </div>
-    </section>
-  )
-}
+const Projects: React.FC = () => (
+  <section id="Projects" className="grid px-3 pt-6 place-items-center ">
+    <Head>
+      <title>Projects</title>
+    </Head>
+    <div className="py-4 md:w-8/12 lg:w-6/12">
+      <h1 className="pb-1 mb-4 text-lg font-bold text-teal-500 border-b-4 border-teal-400">
+        PROJECTS
+      </h1>
+      {projects.map((o, i) => (
+        <ProjectCard
+          key={i}
+          name={o.name}
+          href={o.href}
+          description={o.description}
+          stack={o.stack}
+          src={o.src}
+          vidType={o.vidType}
+        />
+      ))}
+    </div>
+  </section>
+)
 export default Projects
