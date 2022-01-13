2205e4100d661b2e5d70ee7f977c948c897a9d3a
Ali <ali.hussein.pre@gmail.com>
2022-01-13 10:20:04 +0100

    feat: tweak footer UI

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,10 +7,10 @@ const Footer: React.FC = () => {
   return (
     <div
       style={{ zIndex: -1 }}
-      className="sticky bottom-0 z-0 flex items-center justify-center w-full h-10 border-t bg-trueGray-50"
+      className="sticky bottom-0 z-0 flex items-center justify-center w-full h-10 border-t bg-zinc-700"
     >
-      <span className="font-light text-blue-400">
-        @ {new Date().getFullYear()} Ali Hussein
+      <span className="font-light text-zinc-100">
+        @{new Date().getFullYear()} Ali Hussein
       </span>
     </div>
   )
