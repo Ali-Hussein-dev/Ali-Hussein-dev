686fc9f24b329a6fe912bdf7855f2da110cf858c
Ali <ali.hussein.pre@gmail.com>
2022-01-13 10:22:59 +0100

    feat: tweak footer UI

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,9 @@
 //=======================
-const Footer: React.FC = () => {
-  // hooks
-  //--------------------------------------
-  // functions
-  //--------------------------------------
-  return (
-    <div
-      style={{ zIndex: -1 }}
-      className="sticky bottom-0 z-0 flex items-center justify-center w-full h-10 border-t bg-zinc-700"
-    >
-      <span className="font-light text-zinc-100">
-        @{new Date().getFullYear()} Ali Hussein
-      </span>
-    </div>
-  )
-}
+const Footer: React.FC = () => (
+  <footer className="sticky bottom-0 z-[-1] flex items-center justify-center w-full h-10 border-t bg-zinc-700">
+    <span className="font-light text-zinc-100">
+      @{new Date().getFullYear()} Ali Hussein
+    </span>
+  </footer>
+)
 export default Footer
