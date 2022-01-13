7114cfd4382a78e0ac9af7bc7326c4d2dd8cf4bb
Ali <ali.hussein.pre@gmail.com>
2022-01-13 09:25:41 +0100

    test: update test files

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import { render, fireEvent } from '../testUtils'
-import { Home } from '../../src/pages/index'
+import { render } from '../testUtils'
+import Index from '../../src/pages/index'
 
 describe('Home page', () => {
   it('matches snapshot', () => {
-    const { asFragment } = render(<Home />, {})
+    const { asFragment } = render(<Index />, {})
     expect(asFragment()).toMatchSnapshot()
-  })
-
-  it('clicking button triggers alert', () => {
-    const { getByText } = render(<Home />, {})
-    window.alert = jest.fn()
-    fireEvent.click(getByText('Test Button'))
-    expect(window.alert).toHaveBeenCalledWith('working ...')
   })
 })
