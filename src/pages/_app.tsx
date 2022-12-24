325e1fd67dfcba68f387e34c02e0c350007deb3d
Ali <ali.hussein.pre@gmail.com>
2022-12-24 18:27:52 +0100

    style: remove bg image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,6 @@ import Head from 'next/head'
 import Footer from '@/components/Footer'
 import '../css/global.css'
 import NextNProgress from 'nextjs-progressbar'
-import Image from 'next/image'
-import BGImage from '../../public/bg-3.jpg'
 import { Nunito } from '@next/font/google'
 const font = Nunito({ subsets: ['latin', 'latin-ext'] })
 
@@ -26,14 +24,8 @@ const MyApp: React.FC<{ Component: React.FC; pageProps: any }> = ({
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>Ali Hussein</title>
     </Head>
-    <main className="relative min-h-screen text-gray-100 col-center lg:text-xl selection:bg-yellow-900/90 selection:text-slate-100">
+    <main className="relative min-h-screen text-gray-100 bg-black col-center lg:text-xl selection:bg-yellow-900/90 selection:text-slate-100">
       <Header />
-      <Image
-        src={BGImage}
-        className="w-full h-screen object-cover fixed top-0 left-0 z-[-1]"
-        alt="background"
-        loading="eager"
-      />
       <div className="grow">
         <Component {...pageProps} />
       </div>
