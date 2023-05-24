602163e9eda17c03b0b6f973a5aa8a57b8dd5a8a
Ali <ali.hussein.pre@gmail.com>
2023-05-24 10:19:45 +0200

    fix: fix grid background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,8 +16,8 @@ const GridBG = () => (
     <defs>
       <pattern
         id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
-        width="120"
-        height="120"
+        width="200"
+        height="200"
         x="50%"
         y="-1"
         patternUnits="userSpaceOnUse"
@@ -25,7 +25,7 @@ const GridBG = () => (
         <path d="M.5 200V.5H200" fill="none" />
       </pattern>
     </defs>
-    <svg x="50%" y="-1" className="overflow-visible fill-gray-800/20">
+    <svg x="50%" y="-1" className="overflow-visible fill-zinc-800/20">
       <path
         d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
         strokeWidth="0"
