f8de4b10563b6eefed90cfa56d1ec3bc2a880037
Ali <ali.hussein.pre@gmail.com>
2023-08-02 12:24:25 +0200

    chore: migrate blog page

---
+++ src/components/client-components.tsx
@@ -0,0 +1,6 @@
+'use client'
+import { Text, clsx } from '@mantine/core'
+import ReactPlayer from 'react-player'
+import NextNProgress from 'nextjs-progressbar'
+
+export { Text, clsx, ReactPlayer, NextNProgress }
