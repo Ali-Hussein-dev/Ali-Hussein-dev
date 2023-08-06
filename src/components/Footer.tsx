1a85e53cda657c7ced77142b48bc1710c628244a
Ali <ali.hussein.pre@gmail.com>
2023-08-06 12:27:45 +0200

    fix: links are not clickable

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,13 +19,13 @@ const links = [
 
 //=======================
 export const Footer: React.FC = () => (
-  <footer className="bottom-0 w-full py-2 max-w-4xl">
+  <footer className="bottom-0 w-full py-2 max-w-4xl z-10">
     <div className="h-[0.5px] bg-gradient-to-r from-transparent via-zinc-500 to-transparent" />
     <div className="flex flex-col items-center w-full px-2 mx-auto lg:flex-row lg:justify-center lg:w-8/12 ">
       <div className="flex flex-col items-center py-1 sm:justify-between sm:flex-row gap-y-3">
         <div className="flex justify-center w-full gap-x-1 sm:justify-start">
           {links.map((o, i) => (
-            <span key={i} className="p-1 ml-1 rounded-xl">
+            <span key={i} className="p-1 ml-1 rounded-xl hover:font-semibold">
               <a
                 href={o.href}
                 className="text-gray-300"
