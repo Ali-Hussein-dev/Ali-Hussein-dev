5ee927c8cd5172875702dcef81750a40dcf884b3
Ali <ali.hussein.pre@gmail.com>
2023-11-02 12:45:06 +0100

    fix: fix broken UI

--- src/components/project-card-2.tsx
+++ src/components/project-card-2.tsx
@@ -10,7 +10,7 @@ export const ProjectCard: React.FC<{
   imgSrc?: string
   date: string
 }> = ({ imgSrc = '', name, href, description, stack, date }) => (
-  <div className="overflow-hidden rounded z-20 w-full flex-col-start border-zinc-700 border">
+  <div className="overflow-hidden rounded z-[2] w-full flex-col-start border-zinc-800 border">
     <div className="relative aspect-[15/12] w-full">
       <Image
         fill
@@ -22,7 +22,7 @@ export const ProjectCard: React.FC<{
       />
     </div>
     <div className="flex flex-col justify-between px-3 py-3 bg-gradient-to-t from-zinc-800/70 to-black w-full h-[70%]">
-      <div className="">
+      <div className="pb-1 sm:pb-0">
         <h3 className="mb-1 font-bold text-base uppercase text-zinc-200">
           {name}
         </h3>
