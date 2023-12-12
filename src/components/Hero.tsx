ce122072a158c75654eedd18a75f6346ddaa044e
Ali <ali.hussein.pre@gmail.com>
2023-12-12 12:50:46 +0100

    refactor: move comp to seprate file

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,57 +1,25 @@
 import React from 'react'
 import { AnchorButton } from './button'
 
-export const GridBG = () => (
-  <svg
-    className="absolute -inset-[-20] h-full w-full stroke-white/20 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
-    aria-hidden="true"
-  >
-    <defs>
-      <pattern
-        id="983e3e4c-de6d-4c3f-8d64-b9761d1534cc"
-        width="200"
-        height="200"
-        x="50%"
-        y="-1"
-        patternUnits="userSpaceOnUse"
-      >
-        <path d="M.5 200V.5H200" fill="none" />
-      </pattern>
-    </defs>
-    <svg x="50%" y="-1" className="overflow-visible fill-zinc-800/20">
-      <path
-        d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
-        strokeWidth="0"
-      />
-    </svg>
-    <rect
-      width="100%"
-      height="100%"
-      strokeWidth="0"
-      fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"
-    />
-  </svg>
-)
-
 export const Hero = () => {
   return (
     <section className="xs:pb-4 relative grid place-items-center">
       <div>
         <div className="flex-col-center prose h-[95svh]">
-          <div className="mx-auto flex-col-center bg-zinc-950/60 animate-in">
+          <div className="mx-auto flex-col-center bg-zinc-950/60">
             <h1 className="mb-4 text-4xl font-custom md:text-5xl lg:text-6xl  text-center font-black">
               <span className="gradientText block">UX Driven</span>
               <span className="bg-gradient-to-b from-white to-white/70 bg-clip-text text-transparent">
                 Product Engineer
               </span>
             </h1>
-            <p className="mb-6 leading-6 text-zinc-300 md:text-xl text-center max-w-2xl">
+            <div className="mb-6 leading-6 text-zinc-300 md:text-xl text-center max-w-2xl">
               I specialize in crafting web apps with
               <span className="text-zinc-300 block">
                 <strong>clean UI</strong>, <strong>intuitive UX</strong> and{' '}
                 <strong>modern stack</strong>
               </span>
-            </p>
+            </div>
             <AnchorButton href="mailto: ali.hussein.pre@gmail.com">
               Get in touch
             </AnchorButton>
