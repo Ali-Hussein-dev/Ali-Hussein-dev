a6b9ef41d5a82089b592efebd49ba86a559dc834
Ali <ali.hussein.pre@gmail.com>
2024-03-25 17:50:52 +0100

    feat: tweak the about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,5 @@
 import { StylizedImage } from './StylizedImage'
+import { FaSquareXTwitter } from 'react-icons/fa6'
 
 const content = {
   p1: 'I am a self-taught developer deeply passionate about creating exceptional user experiences through intuitive design. With a relentless drive to merge creativity with functionality, my journey in the realm of UX/UI has been both challenging and rewarding.',
@@ -11,22 +12,32 @@ export const About = () => {
       <div className="[background-image:url(/bottom-grid.svg)] bg-no-repeat w-full bg-contain bg-bottom min-h-[40rem] center pb-6">
         <div
           className={
-            'mx-auto flex md:justify-between md:items-center md:flex-row flex-col w-full max-w-6xl gap-4'
+            'mx-auto flex md:justify-between md:items-center md:flex-row flex-col w-full max-w-6xl gap-4 md:px-4'
           }
         >
+          {/* // TEXT SECTION */}
+          <div className="p-4 w-full font-indie-flower order-last md:order-first">
+            <h2 className="title font-indie-flower">About Me</h2>
+            <div className="max-w-2xl text-zinc-300  md:text-xl space-y-2 pt-2">
+              <p className="leading-9 font-indie-flower">{content.p1}</p>
+              <p className="leading-9 font-indie-flower">{content.p2}</p>
+            </div>
+            <div>
+              <a
+                href="https://twitter.com/AliHussein_20"
+                className="inline-block px-6 py-2 mt-4 text-white bg-zinc-900 hover:bg-zinc-800 rounded-md"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Follow on <FaSquareXTwitter className="inline-block" />
+              </a>
+            </div>
+          </div>
           <StylizedImage
             src="/laptop.jpg"
             sizes="(min-width: 1024px) 41rem, 31rem"
             className="justify-center lg:justify-end"
           />
-          {/* // TEXT SECTION */}
-          <div className="p-4 w-full font-indie-flower">
-            <h2 className="title font-indie-flower">About Me</h2>
-            <div className="max-w-2xl text-zinc-300  md:text-xl space-y-4 pt-3">
-              <p className="leading-9 font-indie-flower">{content.p1}</p>
-              <p className="leading-9 font-indie-flower">{content.p2}</p>
-            </div>
-          </div>
         </div>
       </div>
     </div>
